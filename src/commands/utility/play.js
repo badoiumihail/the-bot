6533b875f40135333a432580553f03b2257470de
Mihail Badoiu <badoiumihail@gmail.com>
2024-03-01 12:20:58 +0200

    reorgnized code, started (/) play command

--- src/commands/utility/play.js
+++ src/commands/utility/play.js
@@ -1,9 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js');
+const { createAudioPlayer, NoSubscriberBehavior } = require('@discordjs/voice');
+
+const player = createAudioPlayer({
+  behaviors: {
+    noSubscriber: NoSubscriberBehavior.Pause,
+  },
+});
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Play the provided youtube url'),
+    .addStringOption((option) =>
+      option
+        .setName('url')
+        .setDescription('The url for the youtube video')
+        .setRequired(true)
+    )
+    .setDescription('Play youtube video in voice channel.'),
   async execute(interaction) {
     await interaction.reply('Pong!');
   },
